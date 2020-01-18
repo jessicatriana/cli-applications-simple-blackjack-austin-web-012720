9a476a5e10666745a57968ad852a6c5bbe2e7747
Jessica Triana <jessica.triana.tx@gmail.com>
2020-01-18 22:04:21 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,8 +23,9 @@ def end_game(card_total)
 end
 
 def initial_round
-  deal_card
-  deal_card
+ card_total = 0
+  card_total = card_total + deal_card
+  card_total = card_total + deal_card
   display_card_total(card_total) 
 end
 
